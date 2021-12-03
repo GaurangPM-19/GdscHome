e954993071db46958945c2bbaad48a9d415979ca
Gaurang <gaurang04052002@gmail.com>
2021-12-03 08:18:47 +0530

    added footer by pavithra not merged also same with about us by raj

---
+++ other/c.js
@@ -0,0 +1,16 @@
+$('.postwrapper').slick({
+    slidesToShow: 3.1,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    nextArrow:$('.next'),
+    prevArrow:$('.prev'),
+});
+/*$('.postwrapper1').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    nextArrow:$('.next1'),
+    prevArrow:$('.prev1'),
+});*/
